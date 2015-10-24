896214ef13570b02cdf9430ebc023f6883ac9500
KVKSEST <sergey.stronchinskiy@kraftvaerk.com>
2015-10-25 00:31:22 +0300

    Added player and control

---
+++ BoxMegaShooter/Assets/Scripts/BasicController.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BasicController : MonoBehaviour
+{
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(string.Format("Horizontal = {0}", Input.GetAxis("Horizontal")));
+    }
+}
