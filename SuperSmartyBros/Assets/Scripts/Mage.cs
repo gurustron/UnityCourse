e48798e18f596f0b395006002950e5db6de487bf
gurustron <gurustronpublic@gmail.com>
2016-03-13 03:51:06 +0300

    Field of view for mage

--- SuperSmartyBros/Assets/Scripts/Mage.cs
+++ SuperSmartyBros/Assets/Scripts/Mage.cs
@@ -5,8 +5,10 @@ public class Mage : BaseEnemy
 	public GameObject projectile;
 	public GameObject deathExplosion;
 	[Tooltip("Distance to react on player")]
-	public float visibilityRadius;
-
+	public float detectionRadius;
+	[Tooltip("Field of vision")]
+	public float detectionAngle;
+	
 	private GameObject _player;
 	private bool _enemyIsSeen = false;
 
@@ -34,9 +36,18 @@ public class Mage : BaseEnemy
 		base.Update();
 		if (_player)
 		{
-			var rayDirection = _player.transform.position - transform.position;
-			var hit = Physics2D.Raycast(transform.position, rayDirection, visibilityRadius, NotTransparentLayers);
-			if (hit && hit.transform.position == _player.transform.position)
+			var playerPosition = _player.transform.position;
+			var magePosition = transform.position;
+
+			var rayDirection = playerPosition - magePosition;
+			var hit = Physics2D.Raycast(magePosition, rayDirection, detectionRadius, NotTransparentLayers);
+
+			//var angle2 = Mathf.Abs(Vector3.Angle(magePosition, playerPosition));
+			//var angle = Mathf.Abs(Mathf.Rad2Deg * Mathf.Atan((magePosition.y - playerPosition.y)/(magePosition.x - playerPosition.x)));// 180/Mathf.PI
+
+			var angle = Mathf.Abs(Mathf.Rad2Deg * Mathf.Atan2(magePosition.y - playerPosition.y,magePosition.x - playerPosition.x));// 180/Mathf.PI
+
+			if (hit && hit.transform.position == playerPosition && angle <= detectionAngle)
 			{
 				if (!_enemyIsSeen)
 				{
